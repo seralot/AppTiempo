2860509f4da64784d115ca699e2ef7f12bd10fc0
Sergio <sergioalot@outlook.es>
2019-10-21 14:50:15 +0200

    Load files

---
+++ src/views/detalles.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="details" class="text-center">
+    <buscador @change="cargaCiudad"></buscador>
+    <div v-if="iniciado" >
+      <visor :ciudad="ciudadActual"></visor>
+    </div>
+  </div>
+</template>
+
+<script>
+ export default{
+    data () {
+      return {
+        ciudadActual: null,
+        iniciado: false
+      }
+    },
+    methods: {
+      cargaCiudad (ciudadEncontrada) {
+        this.iniciado = true
+        this.ciudadActual = ciudadEncontrada
+      }
+    }
+}
+</script>
+
+<style lang="scss">
+$tamaño-icono: 4em;
+.icono{
+  width: $tamaño-icono;
+}
+
+</style>
