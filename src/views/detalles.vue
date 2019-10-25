62f42336d1511247538233610ef1ae166dfb1272
Sergio <sergioalot@outlook.es>
2019-10-25 15:07:03 +0200

    hide button

--- src/views/detalles.vue
+++ src/views/detalles.vue
@@ -3,47 +3,59 @@
     <buscador @change="cargaDatos"></buscador>
     <div v-if="iniciado" >
       <visor :datosCiudad="dias"></visor>
-      <button class="mt-2 btn btn-lg btn-outline-dark" @click="ocultar()">Mostrar más</button>
+      <button v-if="this.dias.length>0" class="mt-2 btn btn-lg btn-outline-dark boton" @click="ocultar()">Mostrar más</button>
       <tabla v-show="oculto" :datosCiudad="datosActuales"></tabla>
     </div>
   </div>
 </template>
 
 <script>
- export default{
-    data () {
-      return {
-        oculto: false,
-        datosActuales: null,
-        dias: [],
-        iniciado: false,
-        loaded: false,
-      }
-    },
-    methods: {
-      cargaDatos (detallesBuscador) {        
-        this.iniciado = true
-        this.datosActuales = detallesBuscador
-        this.borrarArray(this.dias);
-        if(detallesBuscador){
-          this.infoVisor();
-        }else{
-          this.dias = [];
-        }
-      },
-      ocultar (){
-        this.oculto = !this.oculto;
-      },
-      infoVisor (){
-        this.dias.push(this.datosActuales[0], this.datosActuales[8], this.datosActuales[16]);
-      },
-      borrarArray(array){
-      if(array.length!=0){
-        array.splice(0, array.length);
-      }
-    },
+export default {
+  data () {
+    return {
+      oculto: false,
+      btn: false,
+      datosActuales: null,
+      dias: [],
+      iniciado: false,
+      loaded: false,
     }
-}
+  },
+  mounted (){
+    if(this.$route.query.ubicacion){
+      this.search(this.$route.query.ubicacion);
+    }
+    this.throttledMethod();
+  },
+  methods: {
+    cargaDatos (detallesBuscador) {        
+      this.iniciado = true
+      this.datosActuales = detallesBuscador
+      this.dias=[]
+      if(detallesBuscador){
+        this.infoVisor();
+      }else{
+        this.dias = [];
+      }
+    },
+    ocultar (){
+      this.oculto = !this.oculto;
+    },
+    ocultarBtn (){
+      if(this.dias!=0){
+        this.btn = !this.btn;
+      }
+    },
+    infoVisor (){
+      this.dias.push(this.datosActuales[0], this.datosActuales[8], this.datosActuales[16]);
+    }
+  },
+  watch: {
+    dias: function(){
+      this.ocultarBtn();
+    }
+  }
+};
 </script>
 
 <style lang="scss">
